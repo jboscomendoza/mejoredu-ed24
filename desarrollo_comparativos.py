ddefe785df82900549125f2d413cdbd8ce8fa206
Juan Bosco Mendoza Vega <jboscomendoza@gmail.com>
2025-07-10 21:28:58 -0600

    Comparativos de desarrollo, página de entrada

---
+++ desarrollo_comparativos.py
@@ -0,0 +1,101 @@
+import streamlit as st
+import polars as pl
+import plotly.graph_objects as go
+from os import path
+
+DES_COLOR = {
+    "Sin evidencia": "#f6511d",
+    "Requiere apoyo": "#ffb400",
+    "En proceso": "#00a6ed",
+    "Desarrollado": "#7fb800",
+}
+
+
+def plot_bar(df: pl.DataFrame, grupo:str, des_color:dict=DES_COLOR) -> go.Figure:
+    n_grupos = len(df[grupo].unique())
+    alto_plot = (n_grupos * 25) + 150
+    plot = go.Figure()
+    plot.add_trace(go.Bar())
+    for desarrollo in df["desarrollo"].unique(maintain_order=True):
+        df_desarrollo = df.filter(pl.col("desarrollo") == desarrollo)
+        plot.add_trace(
+            go.Bar(
+                y=df_desarrollo[grupo],
+                x=df_desarrollo["porcentaje"],
+                orientation="h",
+                name=desarrollo,
+                text=df_desarrollo.select("porcentaje").with_columns(pl.col("porcentaje") * 100)["porcentaje"].round(2),
+                marker=dict(color=des_color[desarrollo]),
+            )
+        )
+    plot.update_layout(
+        barmode="stack",
+        xaxis=dict(tickformat=",.2%"),
+        margin=dict(l=5, t=10),
+        height=alto_plot,
+    )
+    return plot
+
+
+@st.cache_data
+def read_parquet_data(archivo: str):
+    parquet_data = pl.read_parquet(path.join("data", archivo)).with_columns(
+        nivel_grado=(
+            pl.col("nivel").cast(pl.String) + " - " + pl.col("grado").cast(pl.String)
+        )
+    )
+    return parquet_data
+
+
+p_nacional = read_parquet_data("p_nac.parquet")
+p_entidad  = read_parquet_data("p_ent.parquet")
+p_servicio = read_parquet_data("p_ser.parquet")
+p_sexo     = read_parquet_data("p_sex.parquet")
+
+campos    = p_nacional["campo"].unique(maintain_order=True)
+entidades = p_entidad["entidad"].unique(maintain_order=True)
+servicios = p_servicio["servicio"].unique(maintain_order=True)
+sexos     = p_sexo["sexo"].unique(maintain_order=True)
+nivel_grados = p_nacional["nivel_grado"].unique(maintain_order=True)
+
+#### Page ####
+st.title("Resultados de la evaluación diagnóstica 2024-2025")
+
+sel_nivel_grado = st.selectbox("Grado", nivel_grados)
+
+tab_ser, tab_sex, tab_ent = st.tabs(["Servicio", "Sexo", "Entidad"])
+
+#### Resultados por servicio ####
+with tab_ser:
+    st.markdown("## Resultados por servicio")
+    p_ser_sel = p_servicio.filter(pl.col("nivel_grado") == sel_nivel_grado)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_ser_campo = p_ser_sel.filter(pl.col("campo") == campo)
+        plot_ser_campo = plot_bar(p_ser_campo, "servicio")
+        st.plotly_chart(plot_ser_campo, key=f"p_servicio_{campo}")
+
+
+#### Resultados por sexo ####
+with tab_sex:
+    st.markdown("## Resultados por sexo")
+    p_sex_sel = p_sexo.filter(pl.col("nivel_grado") == sel_nivel_grado)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_sex_campo = p_sex_sel.filter(pl.col("campo") == campo)
+        plot_sex_campo = plot_bar(p_sex_campo, "sexo")
+        st.plotly_chart(plot_sex_campo, key=f"p_sexo_{campo}")
+
+
+#### Resultados por entidad ####
+with tab_ent:
+    st.markdown("## Resultados por entidad")
+    p_ent_sel = p_entidad.filter(pl.col("nivel_grado") == sel_nivel_grado)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_ent_campo = p_ent_sel.filter(pl.col("campo") == campo)
+        plot_ent_campo = plot_bar(p_ent_campo, "entidad")
+        st.plotly_chart(plot_ent_campo, key=f"p_entidad_{campo}")
---
+++ desarrollo_general.py
@@ -0,0 +1,111 @@
+import streamlit as st
+import polars as pl
+import plotly.graph_objects as go
+from os import path
+
+DES_COLOR = {
+    "Sin evidencia": "#f6511d",
+    "Requiere apoyo": "#ffb400",
+    "En proceso": "#00a6ed",
+    "Desarrollado": "#7fb800",
+}
+
+
+def plot_bar(df: pl.DataFrame, des_color:dict=DES_COLOR) -> go.Figure:
+    plot = go.Figure()
+    plot.add_trace(go.Bar())
+    for desarrollo in df["desarrollo"].unique(maintain_order=True):
+        df_desarrollo = df.filter(pl.col("desarrollo") == desarrollo)
+        plot.add_trace(
+            go.Bar(
+                x=df_desarrollo["nivel_grado"],
+                y=df_desarrollo["porcentaje"],
+                name=desarrollo,
+                marker=dict(color=des_color[desarrollo]),
+            )
+        )
+    plot.update_layout(
+        barmode="stack",
+        yaxis=dict(tickformat=",.2%"),
+        xaxis_tickangle=90,
+        margin=dict(l=5, t=10),
+        height=300,
+    )
+    return plot
+
+
+@st.cache_data
+def read_parquet_data(archivo: str):
+    parquet_data = pl.read_parquet(path.join("data", archivo)).with_columns(
+        nivel_grado=(
+            pl.col("nivel").cast(pl.String) + " - " + pl.col("grado").cast(pl.String)
+        )
+    )
+    return parquet_data
+
+
+p_nacional = read_parquet_data("p_nac.parquet")
+p_entidad  = read_parquet_data("p_ent.parquet")
+p_servicio = read_parquet_data("p_ser.parquet")
+p_sexo     = read_parquet_data("p_sex.parquet")
+
+campos    = p_nacional["campo"].unique(maintain_order=True)
+entidades = p_entidad["entidad"].unique(maintain_order=True)
+servicios = p_servicio["servicio"].unique(maintain_order=True)
+sexos     = p_sexo["sexo"].unique(maintain_order=True)
+
+#### Page ####
+st.title("Resultados de la evaluación diagnóstica 2024-2025")
+
+tab_nac, tab_ser, tab_sex, tab_ent = st.tabs(["Nacional", "Servicio", "Sexo", "Entidad"])
+
+#### Resultados nacionales ####
+with tab_nac:
+    st.markdown("## Resultados nacionales")
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_nac_campo = p_nacional.filter(pl.col("campo") == campo)
+        plot_nac_campo = plot_bar(p_nac_campo)
+        st.plotly_chart(plot_nac_campo, key=f"p_nacional_{campo}")
+
+#### Resultados por servicio ####
+with tab_ser:
+    st.markdown("## Resultados por servicio")
+
+    sel_servicio= st.selectbox("Servicio", servicios)
+    p_ser_sel = p_servicio.filter(pl.col("servicio") == sel_servicio)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_ser_campo = p_ser_sel.filter(pl.col("campo") == campo)
+        plot_ser_campo = plot_bar(p_ser_campo)
+        st.plotly_chart(plot_ser_campo, key=f"p_servicio_{campo}")
+
+
+#### Resultados por sexo ####
+with tab_sex:
+    st.markdown("## Resultados por sexo")
+
+    sel_sexo = st.selectbox("Sexo", sexos)
+    p_sex_sel = p_sexo.filter(pl.col("sexo") == sel_sexo)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_sex_campo = p_sex_sel.filter(pl.col("campo") == campo)
+        plot_sex_campo = plot_bar(p_sex_campo)
+        st.plotly_chart(plot_sex_campo, key=f"p_sexo_{campo}")
+
+
+#### Resultados por entidad ####
+with tab_ent:
+    st.markdown("## Resultados por entidad")
+
+    sel_entidad = st.selectbox("Entidad", entidades)
+    p_ent_sel = p_entidad.filter(pl.col("entidad") == sel_entidad)
+
+    for campo in campos:
+        st.markdown(f"**{campo}**")
+        p_ent_campo = p_ent_sel.filter(pl.col("campo") == campo)
+        plot_ent_campo = plot_bar(p_ent_campo)
+        st.plotly_chart(plot_ent_campo, key=f"p_entidad_{campo}")
